3c8efd7c104c5208606433c7a1d8a2355c08cf12
thibaudcathala <thibaud.cathala@epitech.eu>
2025-02-08 14:04:55 +0100

    add: end flag that can be see in multiple color

--- the-invisible-path/end_flag.gd
+++ the-invisible-path/end_flag.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+@onready var colorRect = $ColorRect
+@onready var textureRect = $TextureRect
+@onready var label = $Label
+
 func _ready():
 	pass
 
 func _process(delta):
-	pass
+	if Global.color1:
+		colorRect.color = Color(0, 0, 0, 1)
+		textureRect.modulate = Color(0, 0, 0, 1)
+		label.add_theme_color_override("font_color", Color(0, 0, 0, 1))
+	else:
+		colorRect.color = Color(1, 1, 1, 1)
+		textureRect.modulate = Color(1, 1, 1, 1)
+		label.add_theme_color_override("font_color", Color(1, 1, 1, 1))
