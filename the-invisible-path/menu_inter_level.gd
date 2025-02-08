51afc302d7301bfcaad2192a8e526ddc5336b9cb
NicolasAlx <nicolas1.alexandre@epitech.eu>
2025-02-08 10:25:30 +0100

    feat: menu inter level has been added to switch level with a menu

--- the-invisible-path/menu_inter_level.gd
+++ the-invisible-path/menu_inter_level.gd
@@ -1,5 +1,5 @@
 extends Control
 
-func _on_button_pressed(): # Go to first level
+func _on_button_pressed(): # Change scene to next level
 	# get_tree().change_scene_to_file("res://scenes/main.tscn")
 	pass
