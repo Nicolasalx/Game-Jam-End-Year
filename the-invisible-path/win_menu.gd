6927800458388e2201d3986046a76f2d3499af80
NicolasAlx <nicolas1.alexandre@epitech.eu>
2025-02-08 11:45:36 +0100

    feat: add inter menu for the 5 levels in the project

--- the-invisible-path/win_menu.gd
+++ the-invisible-path/win_menu.gd
@@ -1,5 +1,4 @@
 extends Control
 
-func _on_button_pressed(): # Go to first level
-	# get_tree().change_scene_to_file("res://scenes/main.tscn")
-	pass
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/level/level_5_to_4.tscn")
