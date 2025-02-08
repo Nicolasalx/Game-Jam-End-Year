d55025d0a0a5410347266753b218fdcb794c845a
thibaudcathala <thibaud.cathala@epitech.eu>
2025-02-08 10:40:28 +0100

    add: particule spawner

---
+++ the-invisible-path/falling_box.gd
@@ -0,0 +1,21 @@
+extends RigidBody2D
+
+@onready var colorRect = $ColorRect
+
+var is_white = false
+
+func _ready():
+	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("toggle_box"):
+		if !is_white:
+			colorRect.color.r = 1
+			colorRect.color.g = 1
+			colorRect.color.b = 1
+			is_white = true
+		else:
+			colorRect.color.r = 0
+			colorRect.color.g = 0
+			colorRect.color.b = 0
+			is_white = false
