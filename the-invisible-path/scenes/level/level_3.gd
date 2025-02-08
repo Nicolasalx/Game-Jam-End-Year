f83e8dd9b7c56c3f9b0add8b386dec28a38bb209
thibaudcathala <thibaud.cathala@epitech.eu>
2025-02-08 16:54:16 +0100

    add: scene 3 new timer mecanic

--- the-invisible-path/scenes/level/level_3.gd
+++ the-invisible-path/scenes/level/level_3.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 @onready var colorRect = $ColorRect
 @onready var label = $Label
+@onready var labelChrono = $Chrono
+
+const max_time = 25
+var time_left = max_time
 
 var is_white = true
 
 func _ready():
-	pass
+	labelChrono.text = str(time_left)
 
 func _process(delta):
 	if Input.is_action_just_pressed("toggle_box"):
@@ -15,6 +19,7 @@ func _process(delta):
 			colorRect.modulate.g = 0
 			colorRect.modulate.b = 0
 			label.add_theme_color_override("font_color", Color(1, 1, 1, 1))
+			labelChrono.add_theme_color_override("font_color", Color(1, 1, 1, 1))
 			is_white = false
 		else:
 			colorRect.modulate.r = 1.0
@@ -22,4 +27,13 @@ func _process(delta):
 			colorRect.modulate.b = 1.0
 			is_white = true
 			label.add_theme_color_override("font_color", Color(0, 0, 0, 1))
-			
+			labelChrono.add_theme_color_override("font_color", Color(0, 0, 0, 1))
+
+	if !Global.alive:
+		time_left = max_time
+	if time_left > 0:
+		time_left -= delta
+		labelChrono.text = str(int(time_left + 0.5))
+	else:
+		Global.alive = false
+		time_left = max_time
