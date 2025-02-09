cc84564c03cf454c013e5dea6fd9b5c461db3058
thibaudcathala <thibaud.cathala@epitech.eu>
2025-02-09 12:01:53 +0100

    add: spike to level 4

--- the-invisible-path/scenes/peak/peak_1.gd
+++ the-invisible-path/scenes/peak/peak_1.gd
@@ -6,8 +6,8 @@ func _ready():
 	pass
 
 func _process(delta):
-	pass
+	peakText.modulate.a = 1.0 if Global.color1 else 0.5
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	Global.alive = false
-	peakText.modulate.a = 1.0 if Global.color1 else 0.5
+	if (Global.color1):
+		Global.alive = false
--- the-invisible-path/scenes/peak/peak_2.gd
+++ the-invisible-path/scenes/peak/peak_2.gd
@@ -6,8 +6,8 @@ func _ready():
 	pass
 
 func _process(delta):
-	pass
+	peakText.modulate.a = 0.5 if Global.color1 else 1.0
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	Global.alive = false
-	peakText.modulate.a = 0.5 if Global.color1 else 1.0
+	if (!Global.color1):
+		Global.alive = false
